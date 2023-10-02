59e63f2dc777b41826aca7f1330a9b0bfbd4fefd
viniciusrvcruz <viniciusrobertopro@gmail.com>
2023-10-02 19:05:01 -0300

    Adicionei o bootstrap, Header, Footer, Aside e Nav

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,28 @@
+<script>
+import Aside from '../../components/layout/Aside.vue';
+import Nav from '../../components/layout/Nav.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Aside,
+    Nav
+  }
+}
+</script>
+
+
+<template>
+  <main>
+
+    <Nav />
+  </main>
+</template>
+
+<style scoped>
+    main {
+      height: 100%;
+      background-image: linear-gradient(to right, #193748, #1B2C42);
+      padding: 20px 150px;
+    }
+</style>
